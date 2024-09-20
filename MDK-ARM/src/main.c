825c2063397fb812601cc0f0136467faa78285ca
Yury Epishin <yuyepishin@gmail.com>
2024-09-20 17:28:31 +0700

    addl consts again

--- MDK-ARM/src/main.c
+++ MDK-ARM/src/main.c
@@ -403,16 +403,16 @@ static void Demo_GyroConfig(void)
 
   /* Configure Mems L3GD20 */
   L3GD20_InitStructure.Power_Mode = L3GD20_MODE_ACTIVE;
-  L3GD20_InitStructure.Output_DataRate = L3GD20_OUTPUT_DATARATE_1;
+  L3GD20_InitStructure.Output_DataRate = L3GD20_OUTPUT_DATARATE_95HZ;
   L3GD20_InitStructure.Axes_Enable = L3GD20_AXES_ENABLE;
-  L3GD20_InitStructure.Band_Width = L3GD20_BANDWIDTH_4;
+  L3GD20_InitStructure.Band_Width = L3GD20_ODR95_BANDWIDTH_25HZ;
   L3GD20_InitStructure.BlockData_Update = L3GD20_BlockDataUpdate_Continous;
   L3GD20_InitStructure.Endianness = L3GD20_BLE_LSB;
   L3GD20_InitStructure.Full_Scale = L3GD20_FULLSCALE_500; 
   L3GD20_Init(&L3GD20_InitStructure);
   
   L3GD20_FilterStructure.HighPassFilter_Mode_Selection =L3GD20_HPM_NORMAL_MODE_RES;
-  L3GD20_FilterStructure.HighPassFilter_CutOff_Frequency = L3GD20_HPFCF_0;
+  L3GD20_FilterStructure.HighPassFilter_CutOff_Frequency = L3GD20_HPFCF_ODR95_7d2HZ;
   L3GD20_FilterConfig(&L3GD20_FilterStructure) ;
   
   L3GD20_FilterCmd(L3GD20_HIGHPASSFILTER_ENABLE);
